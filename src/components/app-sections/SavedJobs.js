36427a2c476ea8b54430fd58ac64b372238521d5
dpsumner1289 <dpsumner1289@gmail.com>
2020-05-18 15:59:57 -0400

    setup context in SavedJobs

--- src/components/app-sections/SavedJobs.js
+++ src/components/app-sections/SavedJobs.js
@@ -3,8 +3,16 @@ import { SavedContext } from "../../contexts/SavedContext";
 
 const SavedJobs = () => {
   const savedList = useContext(SavedContext);
-  console.dir(savedList);
-  return <div></div>;
+  const savedListingMarkup = savedList.state.savedJobListings.map(
+    savedListing => {
+      return <li>{savedListing.name}</li>;
+    }
+  );
+  return (
+    <div>
+      <ul>{savedListingMarkup}</ul>
+    </div>
+  );
 };
 
 export default SavedJobs;
